308998c3f83fdb14ff356cba7afb01e19532cb32
Kevin Cathcart <kevin.cathcart@captiveaire.com>
2019-08-19 17:33:04 -0400

    Add support for code flow silent-refresh and popup

--- projects/lib/src/types.ts
+++ projects/lib/src/types.ts
@@ -1,14 +1,6 @@
 /**
- * Additional options that can be passt to tryLogin.
+ * Additional options that can be passed to tryLogin.
  */
-export class LoginOptions {
-  /**
-   * Is called, after a token has been received and
-   * successfully validated.
-   *
-   * Deprecated:  Use property ``events`` on OAuthService instead.
-   */
-  onTokenReceived?: (receivedTokens: ReceivedTokens) => void;
 
   /**
    * Hook, to validate the received tokens.
@@ -28,7 +20,12 @@ export class LoginOptions {
   /**
    * A custom hash fragment to be used instead of the
    * actual one. This is used for silent refreshes, to
-   * pass the iframes hash fragment to this method.
+   * pass the iframes hash fragment to this method, and
+   * is also used by popup flows in the same manner.
+   * This can be used with code flow, where is must be set
+   * to a hash symbol followed by the querystring. The
+   * question mark is optional, but may be present following
+   * the hash symbol.
    */
   customHashFragment?: string;
 
@@ -45,9 +42,17 @@ export class LoginOptions {
   /**
    * Normally, you want to clear your hash fragment after
    * the lib read the token(s) so that they are not displayed
-   * anymore in the url. If not, set this to true.
+   * anymore in the url. If not, set this to true. For code flow
+   * this controls removing query string values.
    */
   preventClearHashAfterLogin? = false;
+
+  /**
+   * Set this for code flow if you used a custom redirect Uri
+   * when retrieving the code. This is used internally for silent
+   * refresh and popup flows.
+   */
+  customRedirectUri?: string;
 }
 
 /**
